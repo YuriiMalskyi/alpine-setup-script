3566459ada56fa6bc3206257cda1e8876c9f75b2
Yurii Malskyi <38533495+YuriiMalskyi@users.noreply.github.com>
2025-03-12 09:09:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+# Set root password
+passwd
+
 # Network configuration
 ip link set eth0 up
-apk update
-apk add dhcpcd
-dhcpcd eth0
 
 # APK repositories
 echo "https://dl-cdn.alpinelinux.org/alpine/v3.21/main" >> /etc/apk/repositories
@@ -12,16 +12,15 @@ echo "https://dl-cdn.alpinelinux.org/alpine/v3.21/community" >> /etc/apk/reposit
 apk update --no-cache
 
 # Install necessary packages
-apk add openjdk17-jre nano
+apk add dhcpcd openssh nano openjdk17-jre maven
+dhcpcd eth0
 
 # SSH configuration
+apk add 
 rc-service sshd start
 rc-update add sshd default
 sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
 rc-service sshd restart
-
-# Set root password
-passwd
 
 # Create directories
 mkdir ~/jar
